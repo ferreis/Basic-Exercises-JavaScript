ed6be97ce0eb5b2a1d349984e35494c826ccc7ce
ferreis <rafa_ferreis@hotmail.com>
2024-06-28 15:17:12 -0300

    Completo o desafio do amigo secreto

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,28 +1,37 @@
-let listaAmigos = ["FERREIS", "RICARDO", "FERNANDO", "LUIZ", "CARLOS", "RAFAEL"];
+let listaAmigos = ["Ferreis", "Ricardo", "Fernando", "Luiz", "Carlos", "Rafael"];
 
 function adicionar() {
     let amigo = document.getElementById("nome-amigo");
-    amigo.value = amigo.value.toUpperCase();
+    amigo.value = sentenceCase(amigo.value);
     console.log(amigo.value);
 
-    if (!listaAmigos.includes(amigo.value)) {
+    if (amigo.value == "") {
+        alert("Opção inválida!\nDigite um nome valido para adicionar à lista do sorteio!");
+    }else if(listaAmigos.includes(amigo.value)){
+        alert("Opção inválida!\nEsse nome ja está na lista");
+    }else{
         listaAmigos.push(amigo.value);
     }
 
     amigo.value = "";
     atualizarLista();
-    atualizarSortear();
 }
 
 function sortear() {
-    listaAmigos = embaralharLista(listaAmigos);
+    //Sortear com o minimo de 3 pessoas
+    if (listaAmigos.length <= 2){
+        alert("Opção inválida!\\nDeve ter no mínimo 3 participantes no sorteio.");
+        return;
+    }
+    atualizarSortear();
+    embaralharLista(listaAmigos);
     console.log(listaAmigos);
     let sorteio = document.getElementById('lista-sorteio');
     for (let index = 0; index < listaAmigos.length; index++) {
         if (index == listaAmigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + `${listaAmigos[index]} --> ${listaAmigos[0]}` + '<br/>';
+            sorteio.innerHTML += `${listaAmigos[index]} --> ${listaAmigos[0]}` + '<br/>';
         } else {
-            sorteio.innerHTML = sorteio.innerHTML + `${listaAmigos[index]} --> ${listaAmigos[index+1]}` + '<br/>';
+            sorteio.innerHTML += `${listaAmigos[index]} --> ${listaAmigos[index+1]}` + '<br/>';
         }
     }
 }
@@ -37,10 +46,10 @@ function embaralharLista(lista) {
             lista[randomIndex], lista[tamanhoLista]
         ];
     }
-    return lista;
 }
 
 function atualizarLista() {
+    atualizarSortear();
     let lista = document.getElementById("lista-amigos");
     lista.innerHTML = "";
 
@@ -48,23 +57,33 @@ function atualizarLista() {
         let label = document.createElement("label");
         label.innerText = listaAmigos[index] + ', ';
         label.addEventListener('click', function(){
-            excluirAmigoDaLista();
+            excluirAmigoDaLista(index);
         })
         lista.appendChild(label)
     }
 }
-function excluirAmigoDaLista() {
+function excluirAmigoDaLista(index) {
     console.log("teste do addEventListener"); 
+    listaAmigos.splice(index, 1);
+    atualizarLista();
 }
 function atualizarSortear() {
     let lista = document.getElementById("lista-sorteio");
     lista.innerHTML = "";
 }
 
-
 function reiniciar() {
     listaAmigos = [];
-    document.getElementById("lista-sorteio").innerHTML = "";
-    document.getElementById("lista-amigos").innerHTML = "";
+    atualizarLista();
+}
 
+function sentenceCase(palavra) {
+    // Converte a primeira letra para maiúscula
+    //O método charAt() retorna o caractere especificado a partir de uma string.
+    const primeiraLetra = palavra.charAt(0).toUpperCase();
+
+    // Converte as demais letras para minúsculas
+    const restoDaPalavra = palavra.slice(1).toLowerCase();
+
+    return (primeiraLetra + restoDaPalavra);
 }
